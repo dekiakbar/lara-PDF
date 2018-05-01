adaa749e7250fa8fb6355ac2fb15a127fec19a9b
dekiakbar <dekiakbar@linuxmail.org>
2018-05-01 23:19:48 +0700

    remove vue.js, compile css & js, + jquery datepicker

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,13 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+//datepicker js
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+
+import 'jquery-ui/ui/widgets/datepicker.js';
+
+//window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +21,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+//const app = new Vue({
+//    el: '#app'
+//});
