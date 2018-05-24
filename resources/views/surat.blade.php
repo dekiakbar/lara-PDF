676e0daa8bd8436f953fd7df23082a6131bda8e4
dekiakbar <dekiakbar@linuxmail.org>
2018-05-24 12:47:51 +0700

    tereh

--- resources/views/surat.blade.php
+++ resources/views/surat.blade.php
@@ -7,7 +7,16 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
     <style type="text/css">
         .tab{
-            margin-left: 50px;
+            margin-left: 80px;
+        }
+
+        table{
+            border: 1px solid;
+            margin: auto;
+            width: 100%;
+        }
+        td{
+            border: 1px solid;
         }
     </style>
 <body>
@@ -36,11 +45,32 @@
 
     <p>Memberikan Tugas Kepada :</p>
 
-    <p>Nama<span class="tab">:  DODY GUNAWAN YUSUF S.Si</span></p>
-    <p>NIP<span class="tab">:  196601281994031002</span></p>
-    <p>Jabatan<span class="tab">:  Kepala BPS Kabupaten Sukabumi</span></p>
+    <p>Nama<span style="margin-left: 95px;">:  DODY GUNAWAN YUSUF S.Si</span></p>
+    <p>NIP<span style="margin-left: 108px;">:  196601281994031002</span></p>
+    <p>Jabatan<span style="margin-left: 85px;">:  Kepala BPS Kabupaten Sukabumi</span></p>
     <p>Anggota<span class="tab">:  -  </span></p>
-    <p>Waktu Pelaksanaan<span class="tab">:   27 Pebruari 2018</span></p>
+
+    <p>Tujuan<span style="margin-left: 88px;">: Perjalanan Dinas Dalam Kota Lebih Dari 8 Jam Dalam Rangka Pemeriksaan Hasil Pencacahan Lapangan Pendataan Statistik Pertanian Tanaman Panen Terintegrasi Dengan Metode Kerangka Sampel Area</span></p>
+    
+    <p>Waktu Pelaksanaan<span style="margin-left: 10px;">:   27 Pebruari 2018</span></p>
+    <p>Pembebanan<span style="margin-left: 55px;">:  054.01.06.2895.027.054.524113</span></p>
+
+    <table>
+        <thead>
+            <tr>
+                <td style="width: 50%;">wew</td>
+                <td style="width: 50%;text-align: center;">
+                    <p>Kabupaten Sukabumi, 1 Pebruari 2018</p>
+                    <p>Kepala</p>
+                    <br>
+                    <span style="margin:auto;width: 100%;">
+                        <p style="text-decoration: underline;">Dody Gunawan Yusuf, S.Si</p>
+                        <p style="margin-top: -15px;">NIP. 19660128 199403 1 002</p>
+                    </span>
+                </td>
+            </tr>
+        </thead>
+    </table>
 
     {{-- <footer>
         <p>Jalan Raya Karangtengah KM 14 No 52 Cibadak, Sukabumi</p>
